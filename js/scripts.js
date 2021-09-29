06eef9a5706f1a5db4c640a52fc2ae6b8e7706cd
leonandes92 <leo_alvesfernandes@hotmail.com>
2021-09-29 13:40:04 -0300

    ajustes finais

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Agency v7.0.4 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
